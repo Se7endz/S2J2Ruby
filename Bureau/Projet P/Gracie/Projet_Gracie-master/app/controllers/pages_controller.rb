edfc8f5832b2b7103f3f0aec526b62ef1126dde6
Se7endz <oussama.bendifallahd@gmail.com>
2019-06-10 13:52:57 +0200

    home page creation ouss

---
+++ Bureau/Projet P/Gracie/Projet_Gracie-master/app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+	before_action :authenticate_admin!, only: [:admin_interface]
+  
+  def static_lp
+  end
+
+  def landing_page
+  end
+
+  def home_page
+    
+  end
+  def admin_interface
+  		@users = User.all
+  		@users_number = User.all.count
+  		@users_no_paid = User.where("subscription > '0'").count
+  		#@user_to_pay = User.find(81).subscription
+  		puts "$$" * 100
+  		puts @user_to_pay
+  		puts "$$" * 100
+  		
+  end
+end
